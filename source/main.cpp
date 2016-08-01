35a0402686108e19312bb8624ff42091e04ea0d4
Maggie <maggie.mei@arm.com>
2016-08-01 14:16:29 +0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -13,22 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#include "minar/minar.h"
-#include "security.h"
 #include "simpleclient.h"
-#include "lwipv4_init.h"
-#include "DHT22.h"
 #include <string>
 #include <sstream>
 #include <vector>
 #include <time.h>
+#include "mbed-trace/mbed_trace.h"
+#include "security.h"
 
-using namespace mbed::util;
+#include "mbed.h"
+#include "rtos.h"
+#include "DHT22.h"
 
-Serial &output = get_stdio_serial();
+#define ETHERNET        1
+#define WIFI            2
+#define MESH_LOWPAN_ND  3
+#define MESH_THREAD     4
 
+#define STRINGIFY(s) #s
+
+#if MBED_CONF_APP_NETWORK_INTERFACE == WIFI
+#include "ESP8266Interface.h"
+ESP8266Interface esp(D1, D0);
+#elif MBED_CONF_APP_NETWORK_INTERFACE == ETHERNET
+#include "EthernetInterface.h"
 EthernetInterface eth;
+#elif MBED_CONF_APP_NETWORK_INTERFACE == MESH_LOWPAN_ND
+#define MESH
+#include "NanostackInterface.h"
+LoWPANNDInterface mesh;
+#elif MBED_CONF_APP_NETWORK_INTERFACE == MESH_THREAD
+#define MESH
+#include "NanostackInterface.h"
+ThreadInterface mesh;
+#endif
+
+#ifndef MESH
+// This is address to mbed Device Connector
+#define MBED_SERVER_ADDRESS "coap://api.connector.mbed.com:5684"
+#else
+// This is address to mbed Device Connector
+#define MBED_SERVER_ADDRESS "coaps://[2607:f0d0:2601:52::20]:5684"
+#endif
+
+Serial output(USBTX, USBRX);
 Ticker flipper;
 Ticker flipper_temp;
 Ticker flipper_hum;
@@ -59,7 +87,7 @@ DigitalOut led3(LED3);
  */
 class ButtonResource {
 public:
-    ButtonResource() {
+    ButtonResource(): counter(0) {
         // create ObjectID with metadata tag of '3200', which is 'digital input'
         btn_object = M2MInterfaceFactory::create_object("3200");
         M2MObjectInstance* btn_inst = btn_object->create_object_instance();
@@ -70,7 +98,10 @@ public:
         btn_res->set_operation(M2MBase::GET_ALLOWED);
         // set initial value (all values in mbed Client are buffers)
         // to be able to read this data easily in the Connector console, we'll use a string
-        btn_res->set_value((uint8_t*)"0", 1);
+        btn_res->set_value((uint8_t*)"0", 1);        
+    }
+
+    ~ButtonResource() {
     }
 
     M2MObject* get_object() {
@@ -99,8 +130,9 @@ public:
 
 private:
     M2MObject* btn_object;
-    uint16_t counter = 0;
+    uint16_t counter;
 };
+
 
 /* 
  * Warning function when the temparature gets higher than 32 degrees
@@ -164,7 +196,7 @@ public:
         float temp_value = 0.00, hum_value = 0.00; // used to store the new values
         int error = 0;
 		
-	error = dht22.sample();
+		error = dht22.sample();
         // read successfully
         if (1 == error) {
             // Get the temparature & humidity value from the sensor
@@ -196,40 +228,96 @@ private:
     M2MObject* temphum_object;
 };
 
-void app_start(int /*argc*/, char* /*argv*/[]) {
+// Network interaction must be performed outside of interrupt context
+Semaphore updates(0);
+volatile bool registered = false;
+volatile bool clicked = false;
+osThreadId mainThread;
 
-    //Sets the console baud-rate
+void unregister() {
+    registered = false;
+    updates.release();
+}
+
+void button_clicked() {
+    clicked = true;
+    updates.release();
+}
+
+// debug printf function
+void trace_printer(const char* str) {
+    printf("%s\r\n", str);
+}
+
+// Status indication
+Ticker status_ticker;
+DigitalOut status_led(LED1);
+void blinky() { status_led = !status_led; }
+
+// Entry point to the program
+int main() {
+    status_ticker.attach_us(blinky, 250000);
+
+    // Keep track of the main thread
+    mainThread = osThreadGetId();
+
+    // Sets the console baud-rate
     output.baud(115200);
-    output.printf("In app_start()\r\n");
 
-    // This sets up the network interface configuration which will be used
-    // by LWM2M Client API to communicate with mbed Device server.
-    eth.init();     //Use DHCP
-    if (eth.connect() != 0) {
-        output.printf("Failed to form a connection!\r\n");
+    output.printf("Starting mbed Client example...\r\n");
+
+    mbed_trace_init();
+    mbed_trace_print_function_set(trace_printer);
+
+    NetworkInterface *network_interface = 0;
+    int connect_success = -1;
+#if MBED_CONF_APP_NETWORK_INTERFACE == WIFI
+    output.printf("\n\rUsing WiFi \r\n");
+    output.printf("\n\rConnecting to WiFi..\r\n");
+    connect_success = esp.connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD);
+    network_interface = &esp;
+#elif MBED_CONF_APP_NETWORK_INTERFACE == ETHERNET
+    output.printf("Using Ethernet\r\n");
+    connect_success = eth.connect();
+    network_interface = &eth;
+#endif
+#ifdef MESH
+    output.printf("Using Mesh\r\n");
+    output.printf("\n\rConnecting to Mesh..\r\n");
+    connect_success = mesh.connect();
+    network_interface = &mesh;
+#endif
+    if(connect_success == 0) {
+    output.printf("\n\rConnected to Network successfully\r\n");
+    } else {
+        output.printf("\n\rConnection to Network Failed %d! Exiting application....\r\n", connect_success);
+        return 0;
     }
-    if (lwipv4_socket_init() != 0) {
-        output.printf("Error on lwipv4_socket_init!\r\n");
+    const char *ip_addr = network_interface->get_ip_address();
+    if (ip_addr) {
+        output.printf("IP address %s\r\n", ip_addr);
+    } else {
+        output.printf("No IP address\r\n");
     }
-    output.printf("IP address %s\r\n", eth.getIPAddress());
-    output.printf("Device name %s\r\n", MBED_ENDPOINT_NAME);
 
-    // we create our button temp&hum resources
-    auto button_resource = new ButtonResource();
-    auto temphum_resource = new TempHumResource();
-    led2 = led3 = 1; // turn off the leds
+    // we create our button and temphum resources
+    ButtonResource button_resource;
+    TempHumResource temphum_resource;
+	led2 = led3 = 1; // turn off the leds
 
-    // Unregister button (SW3) press will unregister endpoint from connector.mbed.com
-    unreg_button.fall(&mbed_client, &MbedClient::test_unregister);
+    // On press of SW3 button on K64F board, example application
+    // will call unregister API towards mbed Device Connector
+    //unreg_button.fall(&mbed_client,&MbedClient::test_unregister);
+    unreg_button.fall(&unregister);
 
     // Observation Button (SW2) press will send update of endpoint resource values to connector
-    obs_button.fall(button_resource, &ButtonResource::handle_button_click);
-    
-    // update the temp&hum every two seconds
-    flipper.attach(temphum_resource, &TempHumResource::handle_temphum, 2.0);
+    obs_button.fall(&button_clicked);
+
+	// update the temp&hum every two seconds
+    flipper.attach(&temphum_resource, &TempHumResource::handle_temphum, 2.0);
 	
     // Create endpoint interface to manage register and unregister
-    mbed_client.create_interface();
+    mbed_client.create_interface(MBED_SERVER_ADDRESS, network_interface);
 
     // Create Objects of varying types, see simpleclient.h for more details on implementation.
     M2MSecurity* register_object = mbed_client.create_register_object(); // server object specifying connector info
@@ -240,14 +328,31 @@ void app_start(int /*argc*/, char* /*argv*/[]) {
 
     // Add objects to list
     object_list.push_back(device_object);
-    object_list.push_back(button_resource->get_object());
-    object_list.push_back(temphum_resource->get_object());
+    object_list.push_back(button_resource.get_object());
+    object_list.push_back(temphum_resource.get_object());
 
     // Set endpoint registration object
     mbed_client.set_register_object(register_object);
 
-    // Issue register command.
-    FunctionPointer2<void, M2MSecurity*, M2MObjectList> fp(&mbed_client, &MbedClient::test_register);
-    minar::Scheduler::postCallback(fp.bind(register_object,object_list));
-    minar::Scheduler::postCallback(&mbed_client,&MbedClient::test_update_register).period(minar::milliseconds(25000));
+    // Register with mbed Device Connector
+    mbed_client.test_register(register_object, object_list);
+    registered = true;
+
+    while (true) {
+        updates.wait(25000);
+        if(registered) {
+            if(!clicked) {
+                mbed_client.test_update_register();
+            }
+        }else {
+            break;
+        }
+        if(clicked) {
+           clicked = false;
+            button_resource.handle_button_click();
+        }
+    }
+
+    mbed_client.test_unregister();
+    status_ticker.detach();
 }
